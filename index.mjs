6e654ede14e4fc39de7afa0faa6f49264e245176
charles <charles_spfc@hotmail.com>
2023-08-26 19:04:19 -0300

    Add cors

--- index.mjs
+++ index.mjs
@@ -2,12 +2,15 @@ import express from "express";
 import axios from "axios";
 import api from "./api.mjs";
 import client from "./redisDB.mjs";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT} `);
